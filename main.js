249b42bc97b6c6b75480148829656f8b322c8ca7
Urchenko Nikolai <urchenko2536@gmail.com>
2018-08-24 13:52:36 +0700

    Калькулятор

--- main.js
+++ main.js
@@ -1,104 +1,85 @@
 let num = null;
-let op = null;
+let p = false;
+let lop = null;
 const input = document.getElementById('num-input');
-const backspaseButton = document.getElementById('backspace');
 
-
-backspaseButton.onclick = () => {
+const backspaceButton = document.getElementById('backspace');
+backspaceButton.onclick = () => {
     if (input.value.length === 1) {
         input.value = '0';
     } else {
         input.value = input.value.slice(0, -1);
+        num = 0;
     }
 };
-deleteDigit = () => {
-    [].shift.apply(input.value)
-};
-enterNumber = (n) => {
+
+const enterNumber = (n) => {
     if (input.value === "0") {
         input.value = n;
     } else {
-        input.value = input.value + n;
+        if (p) {
+            num = parseFloat(input.value);
+            input.value = n;
+            p = false
+        } else {
+            input.value = input.value + n;
+        }
     }
 };
 
-const numButtons = document.getElementsByClassName('num-buttons');
-for (let i = 0; i<numButtons.length; i++) {
+const dispatchOperation = (operation) => {
+    if (lop) {
+        if (!p) {
+            num = processOperation(lop, num, parseFloat(input.value));
+            input.value = num;
+        }
+    }
+    p = true;
+    lop = operation;
+};
+
+
+const numButtons = document.getElementsByClassName('num-button');
+for (let i = 0; i < numButtons.length; i++) {
     const button = numButtons[i];
     const num = button.textContent;
-    button.onclick = () => {enterNumber(num)}
+    button.onclick = () => {
+        enterNumber(num)
+    }
 }
 
-const num1 = document.getElementById('num1');
 
-num1.onclick = () => {
-    enterNumber(1);
-};
-
-const num2 = document.getElementById('num2');
-
-num2.onclick = () => {
-    enterNumber(2);
-};
-
-const num3 = document.getElementById('num3');
-
-num3.onclick = () => {
-    enterNumber(3);
-};
-
-const num4 = document.getElementById('num4');
-
-num4.onclick = () => {
-    enterNumber(4);
-};
-const num5 = document.getElementById('num5');
-
-num5.onclick = () => {
-    enterNumber(5);
-};
-const num6 = document.getElementById('num6');
-
-num6.onclick = () => {
-    enterNumber(6);
-};
-const num7 = document.getElementById('num7');
-
-num7.onclick = () => {
-    enterNumber(7);
-};
-const num8 = document.getElementById('num8');
-
-num8.onclick = () => {
-    enterNumber(8);
-};
-const num9 = document.getElementById('num9');
-
-num9.onclick = () => {
-    enterNumber(9);
-};
-const num0 = document.getElementById('num0');
-
-num0.onclick = () => {
-    enterNumber(0);
-};
-
-const tochka = document.getElementById('tochka') ;
-console.log(tochka);
+const tochka = document.getElementById('tochka');
 tochka.onclick = () => {
+    const n = input.value.length;
+    const p = input.value.length;
     for (let i = 0; i < input.value.length; i++) {
-            if (input.value[i] === '.') {
-                return
-            }
+        if (input.value[i] === '.') {
+            return
         }
-        enterNumber('.')
-    };
-const plus = document.getElementById('plus');
-console.log(plus);
-plus.onclick =() => {
-    if (num == null) {
-      op = '+';
-        console.log('+')
+    }
+    input.value('.')
+};
+
+
+const dokButtons = document.getElementsByClassName('dok-button');
+[].forEach.call(dokButtons, (button) => {
+    button.onclick = () => {
+        dispatchOperation(button.textContent);
+    }
+});
+
+
+const processOperation = (operation, argument1, argument2) => {
+    switch (operation) {
+        case '+':
+            return argument1 + argument2;
+        case '-':
+            return argument1 - argument2;
+        case '*':
+            return argument1 * argument2;
+        case '/':
+            return argument1 / argument2;
     }
 };
 
@@ -107,5 +88,4 @@ plus.onclick =() => {
 
 
 
-    //DOM - Document Object Model
-    //BOM - Browser Object Model
+
